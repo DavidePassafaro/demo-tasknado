e9deede8a902f8d3d25a54da3d76df9da412dd76
Davide Passafaro <passafaro@hotmail.it>
2025-12-11 18:45:54 +0100

    feat: add home feature library

---
+++ libs/feature/home/src/pages/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tn-home',
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+  imports: [],
+})
+export class Home {}
