3465bef9b1f53096b940a3e855d43c7da068c20b
Davide Passafaro <passafaro@hotmail.it>
2025-12-11 18:38:49 +0100

    feat: enhance not-found page with primary button

--- libs/feature/not-found/src/pages/not-found/not-found.ts
+++ libs/feature/not-found/src/pages/not-found/not-found.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PrimaryButtonComponent } from '@shared/ui';
 
 @Component({
   selector: 'tn-not-found',
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
-  imports: [RouterModule],
+  imports: [RouterModule, PrimaryButtonComponent],
 })
 export class NotFound {}
