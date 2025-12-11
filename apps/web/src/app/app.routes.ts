01f734b2f0c7758560e2b060b78d0cea5729a00b
Davide Passafaro <passafaro@hotmail.it>
2025-12-11 03:04:58 +0100

    feat: add not-found feature

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '404',
+    loadComponent: () => import('@feature/not-found').then((m) => m.NotFound),
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
---
+++ libs/feature/not-found/src/lib/not-found/not-found.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'tn-not-found',
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss',
+  imports: [RouterModule],
+})
+export class NotFound {}
