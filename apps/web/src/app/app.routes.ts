250ff448fb0d665464b9fb5640a8595a250f2534
Davide Passafaro <passafaro@hotmail.it>
2025-12-11 18:50:36 +0100

    feat: add home route to application routing

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('@feature/home').then((m) => m.Home),
+  },
+  {
     path: '404',
     loadComponent: () => import('@feature/not-found').then((m) => m.NotFound),
   },
