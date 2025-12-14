a595c95b18369e6eb9017aa4e670429ea4ff26df
Davide Passafaro <passafaro@hotmail.it>
2025-12-15 00:46:37 +0100

    feat: refactor project service methods for consistency and type safety

--- libs/feature/projects/src/services/projects.service.ts
+++ libs/feature/projects/src/services/projects.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Project } from '@shared/models';
+import { Observable } from 'rxjs';
 
 const BASE_API_URL = 'http://localhost:4000/';
 
@@ -24,7 +25,7 @@ export class ProjectsService {
       });
   }
 
-  addProject(project: Partial<Project>) {
+  addProject(project: Partial<Project>): void {
     this.http
       .post<Project>(`${BASE_API_URL}api/projects`, project, { withCredentials: true })
       .subscribe((response) => {
@@ -32,20 +33,27 @@ export class ProjectsService {
       });
   }
 
-  getProject(id: number): Project | null {
-    return this.projects().find((p) => p.id === id) || null;
+  getProjectById(id: number): Observable<Project> {
+    return this.http.get<Project>(`${BASE_API_URL}api/projects/${id}`, { withCredentials: true });
   }
 
   updateProject(id: number, updates: Partial<Project>) {
-    const projects = this.projects();
-    const index = projects.findIndex((p) => p.id === id);
-    if (index !== -1) {
-      projects[index] = { ...projects[index], ...updates };
-      this.projects.set([...projects]);
-    }
+    this.http
+      .put<Project>(`${BASE_API_URL}api/projects/${id}`, updates, { withCredentials: true })
+      .subscribe((updatedProject) => {
+        const updatedProjects = this.projects().map((project) =>
+          project.id === id ? updatedProject : project
+        );
+        this.projects.set(updatedProjects);
+      });
   }
 
-  deleteProject(id: number) {
-    this.projects.set(this.projects().filter((p) => p.id !== id));
+  deleteProject(id: number): void {
+    this.http
+      .delete(`${BASE_API_URL}api/projects/${id}`, { withCredentials: true })
+      .subscribe(() => {
+        const updatedProjects = this.projects().filter((p) => p.id !== id);
+        this.projects.set(updatedProjects);
+      });
   }
 }
