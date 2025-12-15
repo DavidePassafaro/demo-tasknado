2162ab8586c7cc5f15772278c3528a5c92a2dd45
Davide Passafaro <passafaro@hotmail.it>
2025-12-15 02:00:58 +0100

    feat: remove unused styles reference from app component

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss',
   imports: [RouterModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
