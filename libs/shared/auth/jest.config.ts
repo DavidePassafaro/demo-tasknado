944b0ac2d0511844c010474df3a8503129d50e7f
Davide Passafaro <passafaro@hotmail.it>
2025-12-17 04:51:55 +0100

    refactor(auth): update displayName in jest.config and clean up tsconfig references

--- libs/shared/auth/jest.config.ts
+++ libs/shared/auth/jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'shared-auth',
+  displayName: 'auth',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../../../coverage/libs/shared/auth',
